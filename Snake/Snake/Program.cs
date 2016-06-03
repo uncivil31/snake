747abbbd0ac0b25969bbccb6a919ffae6fc7e067
vlad <vmaltsev90@mail.ru>
2016-06-03 13:53:20 +0500

    les6

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,21 +10,35 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-           
+            Point p1 = new Point(1, 3, '*');           
             p1.Draw();
 
-            Point p2 = new Point(4, 5, '#');
-            
+            Point p2 = new Point(4, 5, '#');            
             p2.Draw();
 
-            Console.ReadLine();
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-        }
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+
+            pList.Add(p1);
+            pList.Add(p2);
+
+
+            Console.ReadLine();
         }
     }
 }
